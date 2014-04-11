e2ebbd2a91479c1dea628c7e32ffaa5f2540d86e
Roy Pereira <roy@shinyads.com>
2014-04-11 16:58:30 -0400

    added DfpUser

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 // https://developers.google.com/api-client-library/javascript/features/authentication
 // https://github.com/google/google-api-nodejs-client/
 
-function connect(clientId, clientSecret, consumerKey, consumerSecret, redirectUrl) {
+var googleapis = require('googleapis');
+var OAuth2 = googleapis.auth.OAuth2;
 
-	var googleapis = require('googleapis');
-	var OAuth2 = googleapis.auth.OAuth2;
-
-	var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
-
-	var url = oauth2Client.generateAuthUrl({
-  		access_type: 'offline',
-  		scope: 'https://www.googleapis.com/auth/plus.me'
-	});
-}
+var DFP_OAUTH2_SCOPE = 'https://www.google.com/apis/ads/publisher/';
 
 
-module.exports = {
-	connect: connect
+var NodeGoogleDfp = function () {};
+
+NodeGoogleDfp.version = 'v201403';
+NodeGoogleDfp.clientId = '';
+NodeGoogleDfp.clientSecret = '';
+NodeGoogleDfp.redirectUrl = '';
+NodeGoogleDfp.oauth2Client = {};
+
+
+NodeGoogleDfp.prototype.setup = function(clientId, clientSecret, RedirectUrl, appName, dfpVersion) {
+
+	this.clientId = clientId;
+	this.clientSecret = clientSecret;
+	this.redirectUrl = redirectUrl;
+	this.appName = appName;
+
+	if (dfpVersion)
+		this.version = dfpVersion;
+
+	return this;
 };
+
+
+NodeGoogleDfp.prototype.getAuthenticationUrl = function() {
+
+	this.oauth2Client = new OAuth2(this.clientId, this.clientSecret, this.redirectUrl);
+
+	var url = this.oauth2Client.generateAuthUrl({
+  		access_type: 'offline',
+  		scope: DFP_OAUTH2_SCOPE
+	});
+
+	return url;
+};
+
+
+NodeGoogleDfp.prototype.setAuthenticationTokenCode = function(code, callback) {
+	
+	this.oauth2Client = new OAuth2(this.clientId, this.clientSecret, this.redirectUrl);
+
+	this.oauth2Client.getToken(code, function(err, tokens) {
+		this.oauth2Client.setCredentials(tokens);
+		if (callback)
+			callback(tokens);
+	});
+
+	return this;
+};
+
+
+NodeGoogleDfp.prototype.setAuthenticationTokens = function(accessToken, refreshToken) {
+	this.oauth2Client.setCredentials({access_token:accessToken, refresh_token: refreshToken});
+};
+
+
+NodeGoogleDfp.prototype.connect = function(networkCode, clientSecret) {
+
+};
+
+
+module.exports = NodeGoogleDfp;
