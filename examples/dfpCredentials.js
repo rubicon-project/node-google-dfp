46850c7eb605abe93ae204b6a899eea221345c02
Andre Souza <ajsouz@gmail.com>
2014-05-20 18:06:21 -0400

    Adding date & currency helpers

--- examples/dfpCredentials.js
+++ examples/dfpCredentials.js
@@ -1,11 +1,11 @@
 // DfpCredentials
 
-modile.exports = {
-	applicationName : "",                // Your application's name
-	client_id : "", 							       // Your application's DFP ID
+module.exports = {
+  applicationName : "",                // Your application's name
+  client_id : "",                      // Your application's DFP ID
   redirect_url : "",                   // Your application's URL for OAUTH redirections
 
-	networkCode : 0,                     // The publisher's DFP network code
-	client_secret : "",                  // The publisher's OAUTH client secret
-	refresh_token : ""                   // The publisher's OAUTH refresh token
-}
+  networkCode : 0,                     // The publisher's DFP network code
+  client_secret : "",                  // The publisher's OAUTH client secret
+  refresh_token : ""                   // The publisher's OAUTH refresh token
+};
